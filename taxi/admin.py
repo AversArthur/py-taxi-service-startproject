9368da788e21fb3a909bc48e999fb928077df0bd
Arthur <avers.target@gmail.com>
2024-10-16 18:42:00 +0200

    Step 1

--- taxi/admin.py
+++ taxi/admin.py
@@ -7,16 +7,20 @@ from taxi.models import Driver, Car, Manufacturer
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("license_number",)
-    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number",)}),)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
     add_fieldsets = UserAdmin.add_fieldsets + (
         ("Additional info", {"fields": ("license_number",)}),
     )
+
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
     list_display = ["model", "manufacturer"]
     list_filter = ["manufacturer",]
     search_fields = ["model",]
+
 
 @admin.register(Manufacturer)
 class ManufacturerAdmin(admin.ModelAdmin):
--- taxi/models.py
+++ taxi/models.py
@@ -20,7 +20,4 @@ class Driver(AbstractUser):
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(Driver,
-                                on_delete=models.SET_NULL,
-                                null=True, blank=True,
-                                related_name='cars')
+    drivers = models.ManyToManyField(Driver, related_name='cars')
